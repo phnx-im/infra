077d3737dc184e670d0ff0a13ec0cc59523db567
raphaelrobert <git@raphaelrobert.com>
2023-03-10 16:41:11 +0100

    Fix license

--- annotate.sh
+++ annotate.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 reuse download AGPL-3.0-or-later
-reuse annotate -c "Phoenix R&D GmbH <hello@phnx.im>" -l AGPL-3.0-or-later -r * .gitignore
+reuse annotate -c "Phoenix R&D GmbH <hello@phnx.im>" -l AGPL-3.0-or-later --skip-existing -r * .gitignore .github
 reuse lint
