b567166e80fada0e1384365c6839d3cba5ad13e2
raphaelrobert <git@raphaelrobert.com>
2023-03-28 16:58:56 +0900

    Add annotations

--- backend/src/auth_service/client_api/user.rs
+++ backend/src/auth_service/client_api/user.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use crate::{
     auth_service::{credentials::ClientCredential, errors::*, storage_provider_trait::*, *},
     messages::client_as::*,
