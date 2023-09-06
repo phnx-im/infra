50ee01ab0b5bca9a8ffd08d7dad9b9858fecae1e
raphaelrobert <git@raphaelrobert.com>
2023-09-06 15:48:27 +0200

    Fix endpoint for WS

--- server/src/endpoints/mod.rs
+++ server/src/endpoints/mod.rs
@@ -14,7 +14,7 @@ pub const ENDPOINT_DS_GROUPS: &str = "/ds_groups";
 /// QS endpoints
 pub const ENDPOINT_QS: &str = "/qs";
 pub const ENDPOINT_QS_FEDERATION: &str = "/qs_federation";
-pub const ENDPOINT_QS_WS: &str = "/qs/ws/"; // WebSocket endpoints must end with a slash.
+pub const ENDPOINT_QS_WS: &str = "/qs/ws";
 
 /// AS endpoints
 pub const ENDPOINT_AS: &str = "/as";
