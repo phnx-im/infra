cebe106f294b8f04de48df1136d81501f16da315
raphaelrobert <git@raphaelrobert.com>
2024-06-08 12:43:08 +0200

    Add missing licenses

--- applogic/src/api/user/connections.rs
+++ applogic/src/api/user/connections.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use anyhow::Result;
 use phnxtypes::identifiers::{SafeTryInto, UserName};
 
