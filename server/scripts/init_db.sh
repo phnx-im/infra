6c9eb71b9354471ca0479a83db78f672e3490308
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2024-08-01 11:03:28 +0200

    change tests to connect to DB via TLS

--- server/scripts/init_db.sh
+++ server/scripts/init_db.sh
@@ -27,16 +27,23 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 if [[ -z "${SKIP_DOCKER}" ]]
 then
     docker run \
+        -v ./scripts/postgres_test_certs:/etc/postgres_certs:ro \
         -e POSTGRES_USER=${DB_USER} \
         -e POSTGRES_PASSWORD=${DB_PASSWORD} \
         -e POSTGRES_DB=${DB_NAME} \
         -p "${DB_PORT}":5432 \
         -d postgres \
-        postgres -N 1000
+        -N 1000 \
+        -c ssl=on \
+        -c ssl_cert_file=/etc/postgres_certs/server.crt \
+        -c ssl_key_file=/etc/postgres_certs/server.key \
+        -c ssl_ca_file=/etc/postgres_certs/root.crt
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
+export PGSSLMODE="verify-ca"
+export PGSSLROOTCERT="./scripts/postgres_test_certs/root.crt"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
