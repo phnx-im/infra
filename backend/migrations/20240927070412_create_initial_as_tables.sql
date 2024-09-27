5a7522d6f769db1c5e177729c3cbac861c23c43c
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2024-09-27 09:11:16 +0200

    snapshot

---
+++ backend/migrations/20240927070412_create_initial_as_tables.sql
@@ -0,0 +1,10 @@
+-- SPDX-FileCopyrightText: 2023 Phoenix R&D GmbH <hello@phnx.im>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+-- 
+CREATE TABLE as_batched_keys(
+    token_key_id smallint NOT NULL,
+    PRIMARY KEY (token_key_id),
+    voprf_server BYTEA NOT NULL
+);
