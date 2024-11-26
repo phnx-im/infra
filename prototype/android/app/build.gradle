febf0bb7053aa2874e26ee61bf648de439f5563f
raphaelrobert <git@raphaelrobert.com>
2024-11-26 16:32:43 +0100

    ci: Play Store upload

--- prototype/android/app/build.gradle
+++ prototype/android/app/build.gradle
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 1
         versionName = flutter.versionName
     }
 
