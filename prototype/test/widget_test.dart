834470f5833e21204f4299cda2333f9374daddbf
raphaelrobert <raphaelrobert@users.noreply.github.com>
2024-11-27 10:54:13 +0100

    ci: Play Store upload (#214)
    * ci: Play Store upload
    
    * Introduce temp trigger
    
    * Fix publish flow
    
    * Add bundle install
    
    * Fix cargo binstall
    
    * Refactor CI setup
    
    * Tweak CI setup again
    
    * Tweak some more
    
    * Fix relative path
    
    * Check in gradle wrapper
    
    * Revamp actions & warm up gradle tooling
    
    * Final touches

---
+++ prototype/test/widget_test.dart
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+void main() {}
