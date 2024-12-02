19c58b4fd782c0d6deb793881be74eea1fdc1106
raphaelrobert <git@raphaelrobert.com>
2024-12-02 18:26:46 +0100

    Refactor background execution modules

---
+++ applogic/src/background_execution/java_api.rs
@@ -0,0 +1,39 @@
+use jni::{
+    objects::{JClass, JString},
+    sys::jstring,
+    JNIEnv,
+};
+
+use crate::{
+    api::mobile_logging::init_logger, background_execution::processing::retrieve_messages_sync,
+};
+
+use super::IncomingNotificationContent;
+
+/// This methos gets called from the Android Messaging Service
+#[no_mangle]
+pub extern "C" fn Java_im_phnx_prototype_NativeLib_process_1new_1messages(
+    mut env: JNIEnv,
+    _class: JClass,
+    content: JString,
+) -> jstring {
+    init_logger();
+    // Convert Java string to Rust string
+    let input: String = env
+        .get_string(&content)
+        .expect("Couldn't get Java string")
+        .into();
+
+    let incoming_content: IncomingNotificationContent = serde_json::from_str(&input).unwrap();
+
+    // Retrieve messages
+    let batch = retrieve_messages_sync(incoming_content.path);
+
+    let response = serde_json::to_string(&batch).unwrap_or_default();
+
+    // Convert Rust string back to Java string
+    let output = env
+        .new_string(response)
+        .expect("Couldn't create Java string");
+    output.into_raw()
+}
---
+++ applogic/src/background_execution/processing.rs
@@ -0,0 +1,89 @@
+use std::panic::{self, AssertUnwindSafe};
+use tokio::runtime::Builder;
+
+use crate::api::user::User;
+
+use super::{NotificationBatch, NotificationContent};
+
+/// TODO: Debug code to be removed
+pub(crate) fn error_batch(e: String) -> NotificationBatch {
+    NotificationBatch {
+        badge_count: 0,
+        removals: Vec::new(),
+        additions: vec![NotificationContent {
+            identifier: "".to_string(),
+            title: "Error".to_string(),
+            body: e,
+            data: "".to_string(),
+        }],
+    }
+}
+
+/// Wraps with a tokio runtime to block on the async functions
+pub(crate) fn retrieve_messages_sync(path: String) -> NotificationBatch {
+    let result = Builder::new_multi_thread()
+        .thread_name("nse-thread")
+        .enable_all()
+        .build()
+        .map_err(|e| {
+            log::error!("Failed to initialize tokio runtime: {}", e);
+            e.to_string()
+        })
+        .and_then(|runtime| {
+            panic::catch_unwind(AssertUnwindSafe(|| {
+                runtime.block_on(async { retrieve_messages(path).await })
+            }))
+            .map_err(|_| {
+                let e = "Failed to execute async function".to_string();
+                log::error!("{}", e);
+                e
+            })
+        });
+
+    match result {
+        Ok(batch) => batch,
+        Err(e) => error_batch(e),
+    }
+}
+
+/// Load the user and retrieve messages
+pub(crate) async fn retrieve_messages(path: String) -> NotificationBatch {
+    log::info!("Retrieving messages with DB path: {}", path);
+    let user = match User::load_default(path).await {
+        Ok(user) => user,
+        Err(e) => {
+            log::error!("Failed to load user: {}", e);
+            return error_batch(e.to_string());
+        }
+    };
+
+    let notifications = match user.fetch_all_messages().await {
+        Ok(fetched_messages) => {
+            log::info!("All messages fetched");
+            fetched_messages
+                .notifications_content
+                .into_iter()
+                .map(|m| NotificationContent {
+                    title: m.title,
+                    body: m.body,
+                    identifier: "".to_string(),
+                    data: "".to_string(),
+                })
+                .collect()
+        }
+        Err(e) => vec![NotificationContent {
+            identifier: "".to_string(),
+            title: "Error fetching messages".to_string(),
+            body: e.to_string(),
+            data: "".to_string(),
+        }],
+    };
+
+    let badge_count = user.global_unread_messages_count().await;
+
+    NotificationBatch {
+        badge_count,
+        removals: Vec::new(),
+        additions: notifications,
+    }
+}
