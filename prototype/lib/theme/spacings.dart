edcdfd32cbc68638dcf321e17337a74816fc1fb7
boxdot <dimitri@phnx.im>
2024-12-04 14:29:26 +0100

    spacings offsets

--- prototype/lib/theme/spacings.dart
+++ prototype/lib/theme/spacings.dart
@@ -9,8 +9,8 @@ abstract final class Spacings {
   static const double s = 16.0;    // + 4
   static const double sm = 20.0;   // + 4
   static const double m = 24.0;    // + 4
-  static const double l = 32.0;    // + 6
-  static const double xl = 48.0;   // + 6
+  static const double l = 32.0;    // + 8
+  static const double xl = 48.0;   // + 16
   static const double xxl = 64.0;  // + 16
   static const double xxxl = 96.0; // + 32
 }
