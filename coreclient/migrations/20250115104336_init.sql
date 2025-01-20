a16a5c609eba10913d4302109dfb4d19b3f54b87
boxdot <dimitri@phnx.im>
2025-01-20 16:17:08 +0100

    update reuse

--- coreclient/migrations/20250115104336_init.sql
+++ coreclient/migrations/20250115104336_init.sql
@@ -1,3 +1,6 @@
+-- SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
 CREATE TABLE user_creation_state (client_id BLOB PRIMARY KEY, state BLOB NOT NULL);
 
 CREATE TABLE own_client_info (
