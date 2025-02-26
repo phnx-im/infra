3fa7f337293c40f677947289636a128b43a84359
boxdot <dimitri@phnx.im>
2025-02-26 13:41:40 +0100

    REUSE

--- applogic/src/util/file_ring_buffer.rs
+++ applogic/src/util/file_ring_buffer.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use bytes::Buf;
 use memmap2::{MmapMut, MmapOptions};
 use parking_lot::Mutex;
