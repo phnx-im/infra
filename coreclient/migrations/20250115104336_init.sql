3b81dd5cf135c35590023dcb189f68c0d0524069
boxdot <dimitri@phnx.im>
2025-03-07 09:38:24 +0100

    make migrate backwards compatible

--- coreclient/migrations/20250115104336_init.sql
+++ coreclient/migrations/20250115104336_init.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS client_record (
     is_default BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE user_creation_state (client_id BLOB PRIMARY KEY, state BLOB NOT NULL);
+CREATE TABLE IF NOT EXISTS user_creation_state (client_id BLOB PRIMARY KEY, state BLOB NOT NULL);
 
-CREATE TABLE own_client_info (
+CREATE TABLE IF NOT EXISTS own_client_info (
     server_url TEXT NOT NULL,
     qs_user_id BLOB NOT NULL,
     qs_client_id BLOB NOT NULL,
@@ -18,13 +18,13 @@ CREATE TABLE own_client_info (
     as_client_uuid BLOB NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_name TEXT NOT NULL PRIMARY KEY,
     display_name TEXT,
     profile_picture BLOB
 );
 
-CREATE TABLE "groups" (
+CREATE TABLE IF NOT EXISTS "groups" (
     group_id BLOB NOT NULL PRIMARY KEY,
     leaf_signer BLOB NOT NULL,
     identity_link_wrapper_key BLOB NOT NULL,
@@ -32,13 +32,13 @@ CREATE TABLE "groups" (
     pending_diff BLOB
 );
 
-CREATE TABLE client_credentials (
+CREATE TABLE IF NOT EXISTS client_credentials (
     fingerprint BLOB NOT NULL PRIMARY KEY,
     client_id TEXT NOT NULL,
     client_credential BLOB NOT NULL
 );
 
-CREATE TABLE group_membership (
+CREATE TABLE IF NOT EXISTS group_membership (
     client_credential_fingerprint BLOB NOT NULL,
     group_id BLOB NOT NULL,
     client_uuid BLOB NOT NULL,
@@ -57,7 +57,7 @@ CREATE TABLE group_membership (
     PRIMARY KEY (group_id, leaf_index, status)
 );
 
-CREATE TABLE contacts (
+CREATE TABLE IF NOT EXISTS contacts (
     user_name TEXT NOT NULL PRIMARY KEY,
     conversation_id BLOB NOT NULL,
     clients TEXT NOT NULL,
@@ -68,14 +68,14 @@ CREATE TABLE contacts (
     FOREIGN KEY (conversation_id) REFERENCES conversations (conversation_id)
 );
 
-CREATE TABLE partial_contacts (
+CREATE TABLE IF NOT EXISTS partial_contacts (
     user_name TEXT NOT NULL PRIMARY KEY,
     conversation_id BLOB NOT NULL,
     friendship_package_ear_key BLOB NOT NULL,
     FOREIGN KEY (conversation_id) REFERENCES conversations (conversation_id)
 );
 
-CREATE TABLE conversations (
+CREATE TABLE IF NOT EXISTS conversations (
     conversation_id BLOB NOT NULL PRIMARY KEY,
     conversation_title TEXT NOT NULL,
     conversation_picture BLOB,
@@ -92,7 +92,7 @@ CREATE TABLE conversations (
     )
 );
 
-CREATE TABLE conversation_messages (
+CREATE TABLE IF NOT EXISTS conversation_messages (
     message_id BLOB NOT NULL PRIMARY KEY,
     conversation_id BLOB NOT NULL,
     timestamp TEXT NOT NULL,
@@ -106,21 +106,21 @@ CREATE TABLE conversation_messages (
     FOREIGN KEY (conversation_id) REFERENCES conversations (conversation_id) DEFERRABLE INITIALLY DEFERRED
 );
 
-CREATE INDEX conversation_messages_conversation_id_idx ON conversation_messages (conversation_id);
+CREATE INDEX IF NOT EXISTS conversation_messages_conversation_id_idx ON conversation_messages (conversation_id);
 
-CREATE INDEX conversation_messages_timetstamp_idx ON conversation_messages (timestamp);
+CREATE INDEX IF NOT EXISTS conversation_messages_timetstamp_idx ON conversation_messages (timestamp);
 
-CREATE TABLE own_leaf_nodes (
+CREATE TABLE IF NOT EXISTS own_leaf_nodes (
     group_id BLOB PRIMARY KEY,
     leaf_node BLOB NOT NULL
 );
 
-CREATE TABLE signature_keys (
+CREATE TABLE IF NOT EXISTS signature_keys (
     public_key BLOB PRIMARY KEY,
     signature_key BLOB NOT NULL
 );
 
-CREATE TABLE epoch_keys_pairs (
+CREATE TABLE IF NOT EXISTS epoch_keys_pairs (
     group_id BLOB NOT NULL,
     epoch_id BLOB NOT NULL,
     leaf_index INTEGER NOT NULL,
@@ -128,12 +128,12 @@ CREATE TABLE epoch_keys_pairs (
     PRIMARY KEY (group_id, epoch_id, leaf_index)
 );
 
-CREATE TABLE encryption_keys (
+CREATE TABLE IF NOT EXISTS encryption_keys (
     public_key BLOB PRIMARY KEY,
     key_pair BLOB NOT NULL
 );
 
-CREATE TABLE group_data (
+CREATE TABLE IF NOT EXISTS group_data (
     group_id BLOB NOT NULL,
     data_type TEXT NOT NULL CHECK (
         data_type IN (
@@ -154,32 +154,32 @@ CREATE TABLE group_data (
     PRIMARY KEY (group_id, data_type)
 );
 
-CREATE TABLE key_packages (
+CREATE TABLE IF NOT EXISTS key_packages (
     key_package_ref BLOB PRIMARY KEY,
     key_package BLOB NOT NULL
 );
 
-CREATE TABLE proposals (
+CREATE TABLE IF NOT EXISTS proposals (
     group_id BLOB NOT NULL,
     proposal_ref BLOB NOT NULL,
     proposal BLOB NOT NULL,
     PRIMARY KEY (group_id, proposal_ref)
 );
 
-CREATE TABLE psks (psk_id BLOB PRIMARY KEY, psk_bundle BLOB NOT NULL);
+CREATE TABLE IF NOT EXISTS psks (psk_id BLOB PRIMARY KEY, psk_bundle BLOB NOT NULL);
 
-CREATE TABLE qs_verifying_keys (
+CREATE TABLE IF NOT EXISTS qs_verifying_keys (
     domain TEXT PRIMARY KEY,
     verifying_key BLOB NOT NULL
 );
 
-CREATE TABLE queue_ratchets (
+CREATE TABLE IF NOT EXISTS queue_ratchets (
     queue_type TEXT PRIMARY KEY CHECK (queue_type IN ('as', 'qs')),
     queue_ratchet BLOB NOT NULL,
     sequence_number INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE as_credentials (
+CREATE TABLE IF NOT EXISTS as_credentials (
     fingerprint TEXT PRIMARY KEY,
     domain TEXT NOT NULL,
     credential_type TEXT NOT NULL CHECK (
@@ -188,13 +188,13 @@ CREATE TABLE as_credentials (
     credential BLOB NOT NULL
 );
 
-CREATE TABLE leaf_keys (
+CREATE TABLE IF NOT EXISTS leaf_keys (
     verifying_key BLOB NOT NULL PRIMARY KEY,
     leaf_signing_key BLOB NOT NULL,
     identity_link_key BLOB NOT NULL
 );
 
-CREATE TABLE store_notifications (
+CREATE TABLE IF NOT EXISTS store_notifications (
     entity_id BLOB NOT NULL,
     kind INTEGER NOT NULL,
     added BOOLEAN NOT NULL,
@@ -203,7 +203,7 @@ CREATE TABLE store_notifications (
     PRIMARY KEY (entity_id, kind)
 );
 
-CREATE TRIGGER delete_orphaned_data AFTER DELETE ON group_membership FOR EACH ROW BEGIN
+CREATE TRIGGER IF NOT EXISTS delete_orphaned_data AFTER DELETE ON group_membership FOR EACH ROW BEGIN
 -- Delete client credentials if they are not our own and not used in any group.
 DELETE FROM client_credentials
 WHERE
@@ -248,7 +248,7 @@ WHERE
 
 END;
 
-CREATE TRIGGER no_partial_contact_overlap_on_insert BEFORE INSERT ON contacts FOR EACH ROW BEGIN
+CREATE TRIGGER IF NOT EXISTS no_partial_contact_overlap_on_insert BEFORE INSERT ON contacts FOR EACH ROW BEGIN
 SELECT
     CASE
         WHEN EXISTS (
@@ -266,7 +266,7 @@ SELECT
 
 END;
 
-CREATE TRIGGER no_partial_contact_overlap_on_update BEFORE
+CREATE TRIGGER IF NOT EXISTS no_partial_contact_overlap_on_update BEFORE
 UPDATE ON contacts FOR EACH ROW BEGIN
 SELECT
     CASE
@@ -285,7 +285,7 @@ SELECT
 
 END;
 
-CREATE TRIGGER no_contact_overlap_on_insert BEFORE INSERT ON partial_contacts FOR EACH ROW BEGIN
+CREATE TRIGGER IF NOT EXISTS no_contact_overlap_on_insert BEFORE INSERT ON partial_contacts FOR EACH ROW BEGIN
 SELECT
     CASE
         WHEN EXISTS (
@@ -303,7 +303,7 @@ SELECT
 
 END;
 
-CREATE TRIGGER no_contact_overlap_on_update BEFORE
+CREATE TRIGGER IF NOT EXISTS no_contact_overlap_on_update BEFORE
 UPDATE ON partial_contacts FOR EACH ROW BEGIN
 SELECT
     CASE
