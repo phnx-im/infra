phnx-im/infra

commit 151afb7a4ebbcd24459eb0f4c903fb08933c729a

feat: handle app foreground and background transitions (#382)
When the app transitions to the background, we stop the WebSocket
connection and halt message polling. When it returns to the foreground,
we restart the WebSocket connection and resume polling.

This change specifically addresses an issue where, if a message is
received while the app is being moved to the background, no system
notification is generated because the WebSocket remains open briefly.
Additionally, it prevents the WebSocket from repeatedly attempting to
reconnect in the background, reducing excessive error log messages.

applogic/src/api/user_cubit.rs
@@ -98,6 +98,12 @@ impl UiUser {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum AppState {
+    Background,
+    Foreground,
+}
+
 /// Provides access to the logged in user and their profile.
 ///
 /// Also connects to the server websocket and listens to messages. Fetches updates from the server.
@@ -113,6 +119,11 @@ pub struct UserCubitBase {
     state: Arc<RwLock<UiUser>>,
     sinks: Option<Vec<StreamSink<UiUser>>>,
     pub(crate) core_user: CoreUser,
+    #[cfg_attr(
+        any(target_os = "linux", target_os = "macos", target_os = "windows"),
+        expect(dead_code)
+    )]
+    app_state_tx: watch::Sender<AppState>,
     _background_tasks_cancel: DropGuard,
 }
 
@@ -131,16 +142,20 @@ impl UserCubitBase {
         let navigation_state = navigation.subscribe();
         let notification_service = navigation.notification_service.clone();
 
+        let (app_state_tx, app_state) = watch::channel(AppState::Foreground);
+
         let cancel = CancellationToken::new();
         spawn_websocket(
             core_user.clone(),
             navigation_state.clone(),
+            app_state.clone(),
             notification_service.clone(),
             cancel.clone(),
         );
         spawn_polling(
             core_user.clone(),
             navigation_state,
+            app_state,
             notification_service.clone(),
             cancel.clone(),
         );
@@ -149,6 +164,7 @@ impl UserCubitBase {
             state,
             sinks: Some(Default::default()),
             core_user,
+            app_state_tx,
             _background_tasks_cancel: cancel.drop_guard(),
         }
     }
@@ -258,33 +274,53 @@ impl UserCubitBase {
         let contacts = self.core_user.contacts().await?;
         Ok(contacts.into_iter().map(From::from).collect())
     }
+
+    pub fn set_app_state(&self, app_state: AppState) {
+        info!(?app_state, "app state changed");
+        // Note: on Desktop, we consider the app to be always in foreground
+        #[cfg(any(target_os = "android", target_os = "ios"))]
+        let _no_receivers = self.app_state_tx.send(app_state);
+    }
 }
 
 fn spawn_websocket(
     core_user: CoreUser,
     navigation_state: watch::Receiver<NavigationState>,
+    mut app_state: watch::Receiver<AppState>,
     notification_service: NotificationService,
     cancel: CancellationToken,
 ) {
     spawn_from_sync(async move {
         let mut backoff = FibonacciBackoff::new();
         let mut websocket_cancel = cancel.child_token();
-        while let Err(error) = run_websocket(
-            &core_user,
-            &navigation_state,
-            &notification_service,
-            &websocket_cancel,
-            &mut backoff,
-        )
-        .await
-        {
-            let timeout = backoff.next_backoff();
-            info!(%error, retry_in =? timeout, "Websocket failed");
+        loop {
+            let res = run_websocket(
+                &core_user,
+                &navigation_state,
+                &mut app_state,
+                &notification_service,
+                &websocket_cancel,
+                &mut backoff,
+            )
+            .await;
+
+            // stop internal websocket loop and issue a new token
             websocket_cancel.cancel();
             websocket_cancel = cancel.child_token();
-            tokio::time::sleep(timeout).await;
+
+            // wait for app to be foreground
+            let _ = app_state
+                .wait_for(|app_state| matches!(app_state, AppState::Foreground))
+                .await;
+
+            // if websocket failed, retry with backoff
+            if let Err(error) = res {
+                let timeout = backoff.next_backoff();
+                info!(%error, retry_in =? timeout, "Websocket failed");
+
+                tokio::time::sleep(timeout).await;
+            }
         }
-        info!("Websocket handler stopped normally");
     });
 }
 
@@ -292,6 +328,7 @@ fn spawn_websocket(
 async fn run_websocket(
     core_user: &CoreUser,
     navigation_state: &watch::Receiver<NavigationState>,
+    app_state: &mut watch::Receiver<AppState>,
     notification_service: &NotificationService,
     cancel: &CancellationToken,
     backoff: &mut FibonacciBackoff,
@@ -303,11 +340,17 @@ async fn run_websocket(
             cancel.clone(),
         )
         .await?;
+
     loop {
+        let in_background =
+            app_state.wait_for(|app_state| matches!(app_state, AppState::Background));
+
         let event = tokio::select! {
             event = websocket.next() => event,
+            _ = in_background => return Ok(()),
             _ = cancel.cancelled() => return Ok(()),
         };
+
         match event {
             Some(event) => {
                 handle_websocket_message(event, core_user, navigation_state, notification_service)
@@ -322,6 +365,7 @@ async fn run_websocket(
 fn spawn_polling(
     core_user: CoreUser,
     navigation_state: watch::Receiver<NavigationState>,
+    mut app_state: watch::Receiver<AppState>,
     notification_service: NotificationService,
     cancel: CancellationToken,
 ) {
@@ -329,6 +373,10 @@ fn spawn_polling(
     spawn_from_sync(async move {
         let mut backoff = FibonacciBackoff::new();
         loop {
+            let _ = app_state
+                .wait_for(|app_state| matches!(app_state, AppState::Foreground))
+                .await;
+
             let res = tokio::select! {
                 _ = cancel.cancelled() => break,
                 res = user.fetch_all_messages() => res,

app/lib/app.dart
@@ -37,6 +37,9 @@ class _AppState extends State<App> with WidgetsBindingObserver {
   late final StreamSubscription<ConversationId> _openedNotificationSubscription;
   final NavigationCubit _navigationCubit = NavigationCubit();
 
+  final StreamController<AppState> _appStateController =
+      StreamController<AppState>.broadcast();
+
   @override
   void initState() {
     super.initState();
@@ -68,6 +71,7 @@ class _AppState extends State<App> with WidgetsBindingObserver {
   Future<void> _onStateChanged(AppLifecycleState state) async {
     if (state == AppLifecycleState.paused) {
       _log.fine('App is in the background');
+      _appStateController.sink.add(AppState.background);
 
       // iOS only
       if (Platform.isIOS) {
@@ -77,6 +81,9 @@ class _AppState extends State<App> with WidgetsBindingObserver {
           await setBadgeCount(count);
         }
       }
+    } else if (state == AppLifecycleState.resumed) {
+      _log.fine('App is in the foreground');
+      _appStateController.sink.add(AppState.foreground);
     }
   }
 
@@ -118,12 +125,13 @@ class _AppState extends State<App> with WidgetsBindingObserver {
             }
           },
           builder: (context, loadableUser) => loadableUser.user != null
-              // Logged in user is accessible everywhere inside the app after
+              // Logged-in user is accessible everywhere inside the app after
               // the user is loaded
               ? BlocProvider<UserCubit>(
                   create: (context) => UserCubit(
                     coreClient: context.read<CoreClient>(),
                     navigationCubit: context.read<NavigationCubit>(),
+                    appStateStream: _appStateController.stream,
                   ),
                   child: router!,
                 )

app/lib/core/api/user_cubit.dart
@@ -15,7 +15,7 @@ import 'user.dart';
 
 // These functions are ignored because they are not marked as `pub`: `emit`, `handle_websocket_message`, `new`, `process_fetched_messages`, `run_websocket`, `spawn_load`, `spawn_polling`, `spawn_websocket`
 // These types are ignored because they are neither used by any `pub` functions nor (for structs and enums) marked `#[frb(unignore)]`: `NotificationContext`, `UiUserInner`
-// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `fmt`, `fmt`, `fmt`
+// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `clone`, `fmt`, `fmt`, `fmt`, `fmt`
 
 // Rust type: RustOpaqueMoi<flutter_rust_bridge::for_generated::RustAutoOpaqueInner<UiUser>>
 abstract class UiUser implements RustOpaqueInterface {
@@ -45,6 +45,8 @@ abstract class UserCubitBase implements RustOpaqueInterface {
   Future<void> removeUserFromConversation(
       ConversationId conversationId, String userName);
 
+  Future<void> setAppState({required AppState appState});
+
   /// Set the display name and/or profile picture of the user.
   Future<void> setProfile({String? displayName, Uint8List? profilePicture});
 
@@ -54,4 +56,10 @@ abstract class UserCubitBase implements RustOpaqueInterface {
 
   /// Get the user profile of the user with the given [`QualifiedUserName`].
   Future<UiUserProfile?> userProfile(String userName);
+}
+
+enum AppState {
+  background,
+  foreground,
+  ;
 }

app/lib/core/frb_generated.dart
@@ -82,7 +82,7 @@ class RustLib extends BaseEntrypoint<RustLibApi, RustLibApiImpl, RustLibWire> {
   String get codegenVersion => '2.9.0';
 
   @override
-  int get rustContentHash => -2034956985;
+  int get rustContentHash => 170936490;
 
   static const kDefaultExternalLibraryLoaderConfig =
       ExternalLibraryLoaderConfig(
@@ -286,6 +286,9 @@ abstract class RustLibApi extends BaseApi {
       {required UserCubitBase that,
       required ConversationId conversationId,
       required String userName});
+
+  Future<void> crateApiUserCubitUserCubitBaseSetAppState(
+      {required UserCubitBase that, required AppState appState});
 
   Future<void> crateApiUserCubitUserCubitBaseSetProfile(
       {required UserCubitBase that,
@@ -2124,6 +2127,34 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
           );
 
   @override
+  Future<void> crateApiUserCubitUserCubitBaseSetAppState(
+      {required UserCubitBase that, required AppState appState}) {
+    return handler.executeNormal(NormalTask(
+      callFfi: (port_) {
+        final serializer = SseSerializer(generalizedFrbRustBinding);
+        sse_encode_Auto_Ref_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUserCubitBase(
+            that, serializer);
+        sse_encode_app_state(appState, serializer);
+        pdeCallFfi(generalizedFrbRustBinding, serializer,
+            funcId: 58, port: port_);
+      },
+      codec: SseCodec(
+        decodeSuccessData: sse_decode_unit,
+        decodeErrorData: null,
+      ),
+      constMeta: kCrateApiUserCubitUserCubitBaseSetAppStateConstMeta,
+      argValues: [that, appState],
+      apiImpl: this,
+    ));
+  }
+
+  TaskConstMeta get kCrateApiUserCubitUserCubitBaseSetAppStateConstMeta =>
+      const TaskConstMeta(
+        debugName: "UserCubitBase_set_app_state",
+        argNames: ["that", "appState"],
+      );
+
+  @override
   Future<void> crateApiUserCubitUserCubitBaseSetProfile(
       {required UserCubitBase that,
       String? displayName,
@@ -2136,7 +2167,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_opt_String(displayName, serializer);
         sse_encode_opt_list_prim_u_8_strict(profilePicture, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 58, port: port_);
+            funcId: 59, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2161,7 +2192,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_Auto_Ref_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUserCubitBase(
             that, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 59)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 60)!;
       },
       codec: SseCodec(
         decodeSuccessData:
@@ -2192,7 +2223,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_StreamSink_Auto_Owned_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUiUser_Sse(
             sink, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 60, port: port_);
+            funcId: 61, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2221,7 +2252,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
             that, serializer);
         sse_encode_String(userName, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 61, port: port_);
+            funcId: 62, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_opt_box_autoadd_ui_user_profile,
@@ -2246,7 +2277,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_Auto_Ref_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUser(
             that, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 62)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 63)!;
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_Uuid,
@@ -2271,7 +2302,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_Auto_Ref_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUser(
             that, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 63, port: port_);
+            funcId: 64, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_CastedPrimitive_usize,
@@ -2301,7 +2332,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_box_autoadd_ui_user_name(userName, serializer);
         sse_encode_Uuid(clientId, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 64, port: port_);
+            funcId: 65, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData:
@@ -2327,7 +2358,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(dbPath, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 65, port: port_);
+            funcId: 66, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_list_ui_client_record,
@@ -2352,7 +2383,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(path, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 66, port: port_);
+            funcId: 67, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData:
@@ -2391,7 +2422,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_opt_String(displayName, serializer);
         sse_encode_opt_list_prim_u_8_strict(profilePicture, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 67, port: port_);
+            funcId: 68, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData:
@@ -2435,7 +2466,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
             that, serializer);
         sse_encode_opt_box_autoadd_platform_push_token(pushToken, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 68, port: port_);
+            funcId: 69, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2460,7 +2491,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_Auto_Ref_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerUser(
             that, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 69)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 70)!;
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_String,
@@ -2483,7 +2514,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: (port_) {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 70, port: port_);
+            funcId: 71, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2508,7 +2539,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(cacheDir, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 71, port: port_);
+            funcId: 72, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2533,7 +2564,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: (port_) {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 72, port: port_);
+            funcId: 73, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_conversation_details_state,
@@ -2560,7 +2591,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: (port_) {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 73, port: port_);
+            funcId: 74, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_conversation_list_state,
@@ -2588,7 +2619,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_StreamSink_log_entry_Sse(s, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 74, port: port_);
+            funcId: 75, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2619,7 +2650,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         sse_encode_String(userName, serializer);
         sse_encode_Uuid(clientId, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 75, port: port_);
+            funcId: 76, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2644,7 +2675,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(dbPath, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 76, port: port_);
+            funcId: 77, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_unit,
@@ -2669,7 +2700,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: (port_) {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 77, port: port_);
+            funcId: 78, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_home_navigation_state,
@@ -2694,7 +2725,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: () {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_list_prim_u_8_loose(bytes, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 78)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 79)!;
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_String,
@@ -2718,7 +2749,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: () {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(logFile, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 79)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 80)!;
       },
       codec: SseCodec(
         decodeSuccessData:
@@ -2745,7 +2776,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(message, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 80, port: port_);
+            funcId: 81, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_message_content,
@@ -2771,7 +2802,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(string, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 81, port: port_);
+            funcId: 82, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_message_content,
@@ -2796,7 +2827,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: () {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_list_prim_u_8_loose(string, serializer);
-        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 82)!;
+        return pdeCallFfi(generalizedFrbRustBinding, serializer, funcId: 83)!;
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_message_content,
@@ -2820,7 +2851,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
       callFfi: (port_) {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 83, port: port_);
+            funcId: 84, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_String,
@@ -2844,7 +2875,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(cacheDir, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 84, port: port_);
+            funcId: 85, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_String,
@@ -2869,7 +2900,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         final serializer = SseSerializer(generalizedFrbRustBinding);
         sse_encode_String(cacheDir, serializer);
         pdeCallFfi(generalizedFrbRustBinding, serializer,
-            funcId: 85, port: port_);
+            funcId: 86, port: port_);
       },
       codec: SseCodec(
         decodeSuccessData: sse_decode_list_prim_u_8_strict,
@@ -3504,6 +3535,12 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   UuidValue dco_decode_Uuid(dynamic raw) {
     // Codec=Dco (DartCObject based), see doc to use other codecs
     return UuidValue.fromByteList(dco_decode_list_prim_u_8_strict(raw));
+  }
+
+  @protected
+  AppState dco_decode_app_state(dynamic raw) {
+    // Codec=Dco (DartCObject based), see doc to use other codecs
+    return AppState.values[raw as int];
   }
 
   @protected
@@ -4852,6 +4889,13 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
     // Codec=Sse (Serialization based), see doc to use other codecs
     var inner = sse_decode_list_prim_u_8_strict(deserializer);
     return UuidValue.fromByteList(inner);
+  }
+
+  @protected
+  AppState sse_decode_app_state(SseDeserializer deserializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    var inner = sse_decode_i_32(deserializer);
+    return AppState.values[inner];
   }
 
   @protected
@@ -6406,6 +6450,12 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   @protected
+  void sse_encode_app_state(AppState self, SseSerializer serializer) {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    sse_encode_i_32(self.index, serializer);
+  }
+
+  @protected
   void sse_encode_block_element(BlockElement self, SseSerializer serializer) {
     // Codec=Sse (Serialization based), see doc to use other codecs
     switch (self) {
@@ -7731,6 +7781,10 @@ class UserCubitBaseImpl extends RustOpaque implements UserCubitBase {
       RustLib.instance.api
           .crateApiUserCubitUserCubitBaseRemoveUserFromConversation(
               that: this, conversationId: conversationId, userName: userName);
+
+  Future<void> setAppState({required AppState appState}) =>
+      RustLib.instance.api.crateApiUserCubitUserCubitBaseSetAppState(
+          that: this, appState: appState);
 
   /// Set the display name and/or profile picture of the user.
   Future<void> setProfile({String? displayName, Uint8List? profilePicture}) =>

app/lib/core/frb_generated.io.dart
@@ -333,6 +333,9 @@ abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
   UuidValue dco_decode_Uuid(dynamic raw);
 
   @protected
+  AppState dco_decode_app_state(dynamic raw);
+
+  @protected
   BlockElement dco_decode_block_element(dynamic raw);
 
   @protected
@@ -863,6 +866,9 @@ abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
 
   @protected
   UuidValue sse_decode_Uuid(SseDeserializer deserializer);
+
+  @protected
+  AppState sse_decode_app_state(SseDeserializer deserializer);
 
   @protected
   BlockElement sse_decode_block_element(SseDeserializer deserializer);
@@ -1449,6 +1455,9 @@ abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
 
   @protected
   void sse_encode_Uuid(UuidValue self, SseSerializer serializer);
+
+  @protected
+  void sse_encode_app_state(AppState self, SseSerializer serializer);
 
   @protected
   void sse_encode_block_element(BlockElement self, SseSerializer serializer);

commit f888f23a6f3ba3d207a746791090e270de715a09

fix: stopping websocket normally
Websocket handler in user cubit can be stopped normally. In this case,
we have to exit the handler. This regression was introduced in #382.

applogic/src/api/user_cubit.rs
@@ -304,6 +304,11 @@ fn spawn_websocket(
             )
             .await;
 
+            // stop handler on websocket cancellation
+            if let Ok(true) = res {
+                break;
+            }
+
             // stop internal websocket loop and issue a new token
             websocket_cancel.cancel();
             websocket_cancel = cancel.child_token();
@@ -324,7 +329,9 @@ fn spawn_websocket(
     });
 }
 
-/// Normal return means the websocket handler was cancelled
+/// Returns `true` if the websocket was cancelled.
+///
+/// Otherwise, returns `false` or an error.
 async fn run_websocket(
     core_user: &CoreUser,
     navigation_state: &watch::Receiver<NavigationState>,
@@ -332,7 +339,7 @@ async fn run_websocket(
     notification_service: &NotificationService,
     cancel: &CancellationToken,
     backoff: &mut FibonacciBackoff,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<bool> {
     let mut websocket = core_user
         .websocket(
             WEBSOCKET_TIMEOUT.as_secs(),
@@ -347,8 +354,8 @@ async fn run_websocket(
 
         let event = tokio::select! {
             event = websocket.next() => event,
-            _ = in_background => return Ok(()),
-            _ = cancel.cancelled() => return Ok(()),
+            _ = in_background => return Ok(false),
+            _ = cancel.cancelled() => return Ok(true),
         };
 
         match event {