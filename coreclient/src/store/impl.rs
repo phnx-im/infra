1b04495f38a7b76f0639e2dd3dd6766c3abba424
boxdot <dimitri@phnx.im>
2025-04-03 15:27:11 +0200

    refactor: invite users flows as a state machine (#394)

--- coreclient/src/store/impl.rs
+++ coreclient/src/store/impl.rs
@@ -67,6 +67,14 @@ impl Store for CoreUser {
         self.leave_conversation(conversation_id).await
     }
 
+    async fn invite_users(
+        &self,
+        conversation_id: ConversationId,
+        invited_users: &[QualifiedUserName],
+    ) -> StoreResult<Vec<ConversationMessage>> {
+        self.invite_users(conversation_id, invited_users).await
+    }
+
     async fn add_contact(&self, user_name: QualifiedUserName) -> StoreResult<ConversationId> {
         self.add_contact(user_name).await
     }
