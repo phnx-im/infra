a45a586555cec58ba1222c5e3b2b013c5b37da67
boxdot <dimitri@phnx.im>
2025-04-15 14:56:43 +0200

    use vendored protoc compiler

--- protos/build.rs
+++ protos/build.rs
@@ -2,9 +2,15 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use tonic_build::Config;
+
 fn main() {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path().unwrap();
+    let mut config = Config::new();
+    config.protoc_executable(protoc_path);
     tonic_build::configure()
-        .compile_protos(
+        .compile_protos_with_config(
+            config,
             &[
                 "api/auth_service/v1/auth_service.proto",
                 "api/delivery_service/v1/delivery_service.proto",
