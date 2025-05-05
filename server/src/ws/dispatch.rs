ef31d23a64ed96cdca86286aee9ac0cde8919829
boxdot <dimitri@phnx.im>
2025-05-05 18:43:10 +0200

    reuse

--- server/src/ws/dispatch.rs
+++ server/src/ws/dispatch.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrighText: 2023 Phoenix R&D GmbH <hello@phnx.im>
+// SPDX-FileCopyrightText: 2023 Phoenix R&D GmbH <hello@phnx.im>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
