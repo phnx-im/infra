ba7a084415d209a7ebb330fa6bdcfd65d077527c
boxdot <dimitri@phnx.im>
2025-05-07 11:52:17 +0200

    remove unused qs params

--- types/src/messages/client_qs.rs
+++ types/src/messages/client_qs.rs
@@ -7,18 +7,14 @@
 //! TODO: We should eventually factor this module out, together with the crypto
 //! module, to allow re-use by the client implementation.
 
-use mls_assist::openmls::prelude::{KeyPackage, KeyPackageIn, SignaturePublicKey};
-use tls_codec::{TlsDeserializeBytes, TlsSerialize, TlsSize};
+use mls_assist::openmls::prelude::{KeyPackage, KeyPackageIn};
 
 use crate::{
     crypto::{
         RatchetEncryptionKey,
         hpke::ClientIdEncryptionKey,
         kdf::keys::RatchetSecret,
-        signatures::{
-            keys::{QsClientVerifyingKey, QsUserVerifyingKey},
-            signable::Signature,
-        },
+        signatures::keys::{QsClientVerifyingKey, QsUserVerifyingKey},
     },
     identifiers::{QsClientId, QsUserId},
 };
@@ -30,39 +26,9 @@ pub struct QsOpenWsParams {
     pub queue_id: QsClientId,
 }
 
-mod private_mod {
-    #[derive(Default)]
-    pub struct Seal;
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct QsFetchMessagesParams {
-    pub payload: QsFetchMessageParamsTBS,
-    pub signature: Signature, // A signature over the whole request using the queue owner's private key.
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct QsFetchMessageParamsTBS {
-    pub client_id: QsClientId,      // The target queue id.
-    pub sequence_number_start: u64, // The sequence number of the first message we want to fetch.
-    pub max_messages: u64, // The maximum number of messages we'd like to retrieve from the QS.
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct QsFetchMessagesResponse {
-    pub messages: Vec<QueueMessage>,
-    pub remaining_messages: u64,
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct QsQueueUpdate {
-    pub owner_public_key_option: Option<RatchetEncryptionKey>,
-    pub owner_signature_key_option: Option<QsClientVerifyingKey>,
-}
-
 // === User ===
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct CreateUserRecordParams {
     pub user_record_auth_key: QsUserVerifyingKey,
     pub friendship_token: FriendshipToken,
@@ -72,39 +38,28 @@ pub struct CreateUserRecordParams {
     pub initial_ratchet_secret: RatchetSecret,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 #[cfg_attr(test, derive(Clone, PartialEq, Eq))]
 pub struct CreateUserRecordResponse {
     pub user_id: QsUserId,
     pub client_id: QsClientId,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct UpdateUserRecordParams {
     pub sender: QsUserId,
     pub user_record_auth_key: QsUserVerifyingKey,
     pub friendship_token: FriendshipToken,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct UserRecordParams {
-    pub(crate) sender: QsUserId,
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct UserRecordResponse {
-    pub(crate) friendship_token: FriendshipToken,
-    pub(crate) client_records: Vec<ClientRecordResponse>,
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct DeleteUserRecordParams {
     pub sender: QsUserId,
 }
 
 // === Client ===
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct CreateClientRecordParams {
     pub sender: QsUserId,
     pub client_record_auth_key: QsClientVerifyingKey,
@@ -113,13 +68,12 @@ pub struct CreateClientRecordParams {
     pub initial_ratchet_secret: RatchetSecret, // TODO: This can be dropped once we support PCS
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-#[cfg_attr(test, derive(Clone, PartialEq, Eq))]
+#[derive(Debug)]
 pub struct CreateClientRecordResponse {
     pub client_id: QsClientId,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct UpdateClientRecordParams {
     pub sender: QsClientId,
     pub client_record_auth_key: QsClientVerifyingKey,
@@ -127,71 +81,46 @@ pub struct UpdateClientRecordParams {
     pub encrypted_push_token: Option<EncryptedPushToken>,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub struct ClientRecordParams {
-    pub(crate) sender: QsUserId,
-    pub(crate) client_id: QsClientId,
-}
-
-//pub type ClientRecordResponse = QsClientRecord;
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-pub(crate) struct ClientRecordResponse {
-    pub(crate) client_record_auth_key: SignaturePublicKey,
-    pub(crate) queue_encryption_key: RatchetEncryptionKey,
-}
-
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct DeleteClientRecordParams {
     pub sender: QsClientId,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct PublishKeyPackagesParams {
     pub sender: QsClientId,
     pub key_packages: Vec<KeyPackageIn>,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct KeyPackageParams {
     pub sender: FriendshipToken,
 }
 
-#[derive(Debug, TlsSerialize, TlsSize)]
+#[derive(Debug)]
 pub struct KeyPackageResponse {
     pub key_package: KeyPackage,
 }
 
-#[derive(Debug, TlsSize, TlsDeserializeBytes)]
+#[derive(Debug)]
 pub struct KeyPackageResponseIn {
     pub key_package: KeyPackageIn,
 }
 
-#[derive(Debug, TlsDeserializeBytes, TlsSerialize, TlsSize)]
-#[cfg_attr(test, derive(Clone, PartialEq, Eq))]
+#[derive(Debug)]
 pub struct EncryptionKeyResponse {
     pub encryption_key: ClientIdEncryptionKey,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
+#[derive(Debug)]
 pub struct DequeueMessagesParams {
     pub sender: QsClientId,
     pub sequence_number_start: u64,
     pub max_message_number: u64,
 }
 
-#[derive(Debug, TlsSerialize, TlsDeserializeBytes, TlsSize)]
-#[cfg_attr(test, derive(Clone, PartialEq, Eq))]
+#[derive(Debug)]
 pub struct DequeueMessagesResponse {
     pub messages: Vec<QueueMessage>,
     pub remaining_messages_number: u64,
-}
-
-#[derive(Debug)]
-pub enum QsSender {
-    User(QsUserId),
-    Client(QsClientId),
-    FriendshipToken(FriendshipToken),
-    QsUserVerifyingKey(QsUserVerifyingKey),
-    Anonymous,
 }
--- types/src/messages/mod.rs
+++ types/src/messages/mod.rs
@@ -19,7 +19,6 @@ pub mod client_as_out;
 pub mod client_ds;
 pub mod client_ds_out;
 pub mod client_qs;
-pub mod client_qs_out;
 pub mod push_token;
 pub mod welcome_attribution_info;
 
