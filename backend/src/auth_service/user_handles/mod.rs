eb0ff1f4c6f12a59c9d89340a0c3ec5693f5d951
boxdot <dimitri@phnx.im>
2025-05-22 15:05:26 +0200

    reuse

--- backend/src/auth_service/user_handles/mod.rs
+++ backend/src/auth_service/user_handles/mod.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
 use displaydoc::Display;
 use phnxtypes::{
     credentials::keys::HandleVerifyingKey,
--- backend/src/auth_service/user_handles/persistence.rs
+++ backend/src/auth_service/user_handles/persistence.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
 use sqlx::{PgExecutor, PgPool, query, query_scalar};
 
 use super::*;
