0f15c810f264222c901642c3e1ec9d7e2473d53d
raphaelrobert <raphaelrobert@users.noreply.github.com>
2025-05-28 15:03:30 +0200

    MacOS badge count (#506)

---
+++ app/lib/background_service.dart
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:prototype/core/core_client.dart';
+import 'package:prototype/util/platform.dart';
+
+class BackgroundService {
+  final CoreClient _coreClient = CoreClient();
+  Timer? _timer;
+
+  void start({bool runImmediately = false}) {
+    if (runImmediately) _performTask();
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) => _performTask());
+  }
+
+  void stop() {
+    _timer?.cancel();
+  }
+
+  void _performTask() async {
+    // Set the badge count on macOS
+    if (Platform.isMacOS) {
+      // Make sure the user is logged in
+      if (_coreClient.maybeUser case final user?) {
+        final count = await user.globalUnreadMessagesCount;
+        await setBadgeCount(count);
+      }
+    }
+  }
+}
