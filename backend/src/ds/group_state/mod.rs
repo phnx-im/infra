e721c0eab6d1014b2bf84480c195bc6a76cb041a
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2025-06-18 09:37:15 +0200

    remove unnecessary serde derives

--- backend/src/ds/group_state/mod.rs
+++ backend/src/ds/group_state/mod.rs
@@ -29,7 +29,6 @@ use phnxcommon::{
     messages::client_ds::WelcomeInfoParams,
     time::TimeStamp,
 };
-use serde::{Deserialize, Serialize};
 use sqlx::PgExecutor;
 use thiserror::Error;
 use tls_codec::{Serialize as _, TlsDeserializeBytes, TlsSerialize, TlsSize};
@@ -42,7 +41,7 @@ use super::{GROUP_STATE_EXPIRATION, ReservedGroupId, process::ExternalCommitInfo
 
 pub(super) mod persistence;
 
-#[derive(Debug, Serialize, Deserialize, TlsSize, TlsDeserializeBytes, TlsSerialize)]
+#[derive(Debug, TlsSize, TlsDeserializeBytes, TlsSerialize)]
 pub(super) struct MemberProfile {
     pub(super) leaf_index: LeafNodeIndex,
     pub(super) client_queue_config: QsReference,
@@ -260,14 +259,14 @@ impl StorableDsGroupData {
     }
 }
 
-#[derive(Serialize, Deserialize, TlsSize, TlsDeserializeBytes, TlsSerialize)]
+#[derive(TlsSize, TlsDeserializeBytes, TlsSerialize)]
 pub(crate) struct SerializableDsGroupStateV1 {
     group_id: GroupId,
     serialized_provider: Vec<u8>,
     member_profiles: Vec<(LeafNodeIndex, MemberProfile)>,
 }
 
-#[derive(Serialize, Deserialize, TlsSize, TlsDeserializeBytes, TlsSerialize)]
+#[derive(TlsSize, TlsDeserializeBytes, TlsSerialize)]
 pub(crate) struct SerializableDsGroupStateV2 {
     group_id: GroupId,
     serialized_provider: Vec<u8>,
@@ -341,7 +340,7 @@ impl SerializableDsGroupStateV2 {
     }
 }
 
-#[derive(Serialize, Deserialize, TlsSize, TlsDeserializeBytes, TlsSerialize)]
+#[derive(TlsSize, TlsDeserializeBytes, TlsSerialize)]
 #[repr(u8)]
 pub(super) enum EncryptableDsGroupState {
     V1(SerializableDsGroupStateV1),
