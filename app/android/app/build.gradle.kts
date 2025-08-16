4d62b7a79f33a686317355465b6a69882a76d692
raphaelrobert <git@raphaelrobert.com>
2025-08-16 13:30:42 +0200

    fix formatting

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         versionName = flutter.versionName
     }
 
-     buildTypes {
+    buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
