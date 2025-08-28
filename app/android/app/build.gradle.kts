392f349c9c1cd4faf0725b52d10cd1f296dffc81
boxdot <dimitri@phnx.im>
2025-08-28 17:40:21 +0200

    build(android): sign app with flutter
    Gradle is not used directly anymore, which allows to build the app for
    single architecture only.
    
    This commit has a side effect that building the app for android with
    `flutter build` will fail if the signing is not setup.

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -11,6 +14,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-messaging")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -38,11 +47,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- app/fastlane/android.rb
+++ app/fastlane/android.rb
@@ -4,23 +4,30 @@ platform :android do
       # Package name
       package_name = "ms.air"
       track = "internal"
-      gradle_propperties = {}
-  
+
       # Determine if we should deploy to the Play Store
       upload_to_play_store = options[:upload_to_play_store]
-  
+
       # We need to wrap the whole process in a begin/rescue block to ensure that we clean up the temporary files
       begin
         # We prepare the keystore and the Play Store key
         if upload_to_play_store
           # Decode the keystore from the base64 string and save it to a temporary file
-          keystore_path = "release-key.jks"
+          keystore_path = "../android/app/release-key.jks"
           base64_keystore = ENV["ANDROID_KEYSTORE_BASE64"]
           UI.user_error!("ANDROID_KEYSTORE_BASE64 environment variable is missing!") if base64_keystore.nil?
           File.open(keystore_path, "wb") do |file|
             file.write(Base64.decode64(base64_keystore))
           end
-  
+
+          # Prepare the signing properties
+          File.open("../android/key.properties", "w") do |file|
+            file.write("storeFile=#{File.expand_path(keystore_path)}\n")
+            file.write("storePassword=#{ENV["ANDROID_KEY_PASSWORD"]}\n")
+            file.write("keyAlias=upload\n")
+            file.write("keyPassword=#{ENV["ANDROID_KEY_PASSWORD"]}\n")
+          end
+
           # Decode the Play Store key from the base64 string and save it to a temporary file
           playstore_key_path = "playstorekey.json"
           base64_playstore_key = ENV["ANDROID_PLAYSTORE_KEY_JSON"]
@@ -28,7 +35,7 @@ platform :android do
           File.open(playstore_key_path, "wb") do |file|
             file.write(Base64.decode64(base64_playstore_key))
           end
-          
+
           # Get the previous build number
           begin
             previous_build_number = google_play_track_version_codes(
@@ -41,38 +48,20 @@ platform :android do
             previous_build_number = 0
           end
           current_build_number = previous_build_number + 1
-  
+
           # Increment the build number in the gradle file
           increment_version_code(
             gradle_file_path: "android/app/build.gradle.kts",
             version_code: current_build_number
           )
-
-          # Prepare the signing properties
-          gradle_propperties = {
-            "android.injected.signing.store.file" => File.expand_path(keystore_path),
-            "android.injected.signing.store.password" => ENV["ANDROID_KEY_PASSWORD"],
-            "android.injected.signing.key.alias" => "upload",
-            "android.injected.signing.key.password" => ENV["ANDROID_KEY_PASSWORD"]
-          }
         end
-     
+
+        build_target = upload_to_play_store ? "appbundle" : "apk"
+
         # We build the app with Flutter first to set up gradle
         sh "flutter precache --android"
         sh "flutter pub get"
-        if upload_to_play_store
-          sh "flutter build appbundle --release"
-        else
-          # Faster build for only one architecture
-          sh "flutter build appbundle --target-platform android-arm64"
-        end
-
-        gradle(
-          task: "bundle",
-          build_type: "Release",
-          project_dir: File.expand_path("../android"),
-          properties: gradle_propperties
-        )
+        sh "flutter build #{build_target} --release --target-platform android-arm64"
 
         if upload_to_play_store
           # Upload to Google Play Store
@@ -86,7 +75,7 @@ platform :android do
             package_name: package_name,
           )
         end
-  
+
       rescue => e
         UI.error(e)
         raise
