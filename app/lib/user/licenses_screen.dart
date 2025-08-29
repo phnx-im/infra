52bf3bb32170cd2dcd21f5869566366f7d84c586
boxdot <dimitri@phnx.im>
2025-08-29 12:47:45 +0200

    reuse

--- app/lib/user/licenses_screen.dart
+++ app/lib/user/licenses_screen.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'package:air/l10n/l10n.dart';
 import 'package:air/theme/theme.dart';
 import 'package:air/widgets/widgets.dart';
