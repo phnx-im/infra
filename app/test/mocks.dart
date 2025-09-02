bf67a02cb94b1cdb0210061a7a35b10433ba629d
boxdot <dimitri@phnx.im>
2025-09-02 15:30:43 +0200

    remove unused mock

--- app/test/mocks.dart
+++ app/test/mocks.dart
@@ -125,5 +125,3 @@ class MockAttachmentsRepository extends Mock implements AttachmentsRepository {}
 
 class MockUserSettingsCubit extends MockCubit<UserSettings>
     implements UserSettingsCubit {}
-
-class MockUiRoomState extends Mock implements UiRoomState {}
