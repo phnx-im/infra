64c0d4ac91ae5e4aaa123963fbab170cf4b12772
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2025-09-08 15:02:06 +0200

    chore(breaking): remove backwards compatibility code (#662)

--- backend/src/auth_service/connection_package/mod.rs
+++ backend/src/auth_service/connection_package/mod.rs
@@ -2,61 +2,37 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use aircommon::messages::connection_package::{ConnectionPackage, legacy::ConnectionPackageV1};
+use aircommon::messages::connection_package::ConnectionPackage;
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
 
 pub(crate) mod persistence;
 
 #[derive(Deserialize)]
 pub(in crate::auth_service) enum StorableConnectionPackage {
-    // This is here so we successfully deserialize old connection packages.
-    #[allow(dead_code)]
-    #[serde(rename = "CurrentVersion")]
-    V1(ConnectionPackageV1),
-    V2(ConnectionPackage),
+    V1(ConnectionPackage),
 }
 
-#[derive(Debug, Error)]
-pub(in crate::auth_service) enum ConnectionPackageStorageError {
-    #[error("Invalid connection package version: {}", actual)]
-    InvalidVersion { actual: String },
-}
-
-impl From<ConnectionPackageStorageError> for sqlx::Error {
-    fn from(error: ConnectionPackageStorageError) -> Self {
-        sqlx::Error::Decode(error.into())
-    }
-}
-
-impl TryFrom<StorableConnectionPackage> for ConnectionPackage {
-    type Error = ConnectionPackageStorageError;
-
-    fn try_from(connection_package: StorableConnectionPackage) -> Result<Self, Self::Error> {
+impl From<StorableConnectionPackage> for ConnectionPackage {
+    fn from(connection_package: StorableConnectionPackage) -> Self {
         match connection_package {
-            StorableConnectionPackage::V2(connection_package) => Ok(connection_package),
-            StorableConnectionPackage::V1(_) => {
-                Err(ConnectionPackageStorageError::InvalidVersion {
-                    actual: "V1".to_string(),
-                })
-            }
+            StorableConnectionPackage::V1(connection_package) => connection_package,
         }
     }
 }
 
 impl From<ConnectionPackage> for StorableConnectionPackage {
     fn from(connection_package: ConnectionPackage) -> Self {
-        StorableConnectionPackage::V2(connection_package)
+        StorableConnectionPackage::V1(connection_package)
     }
 }
 
 #[derive(Serialize)]
 pub(in crate::auth_service) enum StorableConnectionPackageRef<'a> {
-    V2(&'a ConnectionPackage),
+    V1(&'a ConnectionPackage),
 }
 
 impl<'a> From<&'a ConnectionPackage> for StorableConnectionPackageRef<'a> {
     fn from(connection_package: &'a ConnectionPackage) -> Self {
-        StorableConnectionPackageRef::V2(connection_package)
+        StorableConnectionPackageRef::V1(connection_package)
     }
 }
