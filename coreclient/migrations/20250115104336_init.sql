385c9a60c126672ca312fafab5fe6d662a88a533
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2025-09-10 17:33:03 +0200

    switch trigger to users table

--- coreclient/migrations/20250115104336_init.sql
+++ coreclient/migrations/20250115104336_init.sql
@@ -358,12 +358,12 @@ SELECT
 
 END;
 
-CREATE TRIGGER IF NOT EXISTS delete_keys AFTER DELETE ON contacts FOR EACH ROW BEGIN
--- Delete user profile keys if the corresponding contact is deleted. Since key
+CREATE TRIGGER IF NOT EXISTS delete_keys AFTER DELETE ON users FOR EACH ROW BEGIN
+-- Delete user profile keys if the corresponding user profile is deleted. Since key
 -- indexes include the user id in their derivation, they are unique per user
 -- and we don't need to check if they are used by another user (or ourselves).
 DELETE FROM indexed_keys
 WHERE
-    fingerprint = OLD.user_profile_key_index;
+    key_index = OLD.decryption_key_index;
 
 END;
